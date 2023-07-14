7992a518d78675aa828b5b8b768ebc1f2a9ccb68
Salih Polat <97470567+SalihPolat32@users.noreply.github.com>
2023-07-14 13:15:47 +0300

    14.07.2023 Gün Sonu

--- Java10jdbcMaven/src/main/java/com/bilgeadam/Test.java
+++ Java10jdbcMaven/src/main/java/com/bilgeadam/Test.java
@@ -2,7 +2,10 @@ package com.bilgeadam;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import javax.sound.midi.Soundbank;
 
 public class Test {
 
@@ -18,13 +21,41 @@ public class Test {
 			
 			// Class.forName("org.postgresql.Driver");
 			connection = DriverManager.getConnection(url, username, password);
+			
 			System.out.println("Bağlantı Başarılı");
-
+			
+			String sql = "INSERT INTO categories (category_name) VALUES ('Eclipse'), ('Eclipse2')";
+			String sql2 = "CREATE TABLE IF NOT EXISTS public.ogrenci\r\n"
+					+ "(\r\n"
+					+ "    id integer NOT NULL DEFAULT nextval('ogrenci_id_seq'::regclass),\r\n"
+					+ "    isim character varying(50) COLLATE pg_catalog.\"default\" NOT NULL,\r\n"
+					+ "    yas smallint,\r\n"
+					+ "    kayit_tarihi date DEFAULT CURRENT_DATE,\r\n"
+					+ "    CONSTRAINT ogrenci_pkey PRIMARY KEY (id)\r\n"
+					+ ")";
+			
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			
+			int count = preparedStatement.executeUpdate();
+			
+			System.out.println(count);
+			
 		} catch (SQLException e) {
 
 			System.out.println("Bağlantı Başarısız!!!");
 			e.printStackTrace();
 			
+		} finally {
+			
+			try {
+				
+				connection.close();
+				
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+				
+			}
 		}
 	}
 }
