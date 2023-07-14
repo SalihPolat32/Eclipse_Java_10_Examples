30a7a2ca4b945181d25e81cc79a3ee5a8396b369
Salih Polat <97470567+SalihPolat32@users.noreply.github.com>
2023-07-14 12:55:41 +0300

    14.07.2023 Gün Sonu

---
+++ Java10jdbcGradle/lib/src/main/java/_Java10jdbcGradle/Test.java
@@ -0,0 +1,30 @@
+package _Java10jdbcGradle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:postgresql://localhost:5432/java10NorthwindD";
+		String username = "postgres";
+		String password = "123456789";
+
+		Connection connection = null;
+
+		try {
+			
+			// Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Bağlantı Başarılı");
+
+		} catch (SQLException e) {
+
+			System.out.println("Bağlantı Başarısız!!!");
+			e.printStackTrace();
+			
+		} 
+	}
+}
